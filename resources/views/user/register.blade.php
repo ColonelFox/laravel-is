c09d588acf3dba0fc9223a2a23bb7147a58d7f49
ColonelFox <dacanay.aj.06@gmail.com>
2023-03-15 15:27:35 +0800

    1st Commit

---
+++ resources/views/user/register.blade.php
@@ -0,0 +1,33 @@
+@include('partials.header')
+<html>
+    <head>
+        <title>Register</title>
+    </head>
+
+<form action="/store" method="Post">
+    @csrf
+  <div class="mb-3">
+    <label for="name" class="form-label">Name:</label>
+        <input
+            type="text" 
+            class="form-control" 
+            name="name">
+  </div>
+  <div class="mb-3">
+    <label for="emailAddress" class="form-label">Email address</label>
+    <input type="email" class="form-control" name="email" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" name="password">
+  </div>
+  <div class="mb-3">
+    <label for="confirmPassword" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" name="password_confirmation">
+  </div>
+  <button type="submit" class="btn btn-primary">Register</button>
+</form>
+
+</html>
+@include('partials.footer')
